5a573f46c0046130aba4cf6193b09eac01c918d5
sagem007 <fly-sanya@yandex.ru>
2016-09-04 15:17:55 +0500

    Commit from Desktop

--- GPSView/Controllers/DevicesController.cs
+++ GPSView/Controllers/DevicesController.cs
@@ -15,17 +15,14 @@ namespace GPSView.Controllers
         public async Task<IHttpActionResult> addDevice(Models.Device device)
         {
             var dm = new GPSView.Models.DB_Model();
-
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
             if (dm.Device.Where(x => x.IMEI == device.IMEI).Count() > 0)
             {
                 return BadRequest("Device is already exists.");
             }
-
             dm.Device.Add(device);
             var add_res = await dm.SaveChangesAsync();
 
@@ -33,7 +30,6 @@ namespace GPSView.Controllers
             {
                 return BadRequest("Device is not added.");
             }
-
             return Created("", device);
         }
     }
